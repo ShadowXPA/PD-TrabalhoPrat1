6b5769e6f079bbf70d9ed8d7314abd5ec2b22a38
ShadøwXPA <pedro.aves@hotmail.com>
2020-10-26 10:34:48 +0000

    Initial commit, client and server project

---
+++ Client/src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module pt.isec.deis.lei.pd.trabprat.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens pt.isec.deis.lei.pd.trabprat.client to javafx.fxml;
+    exports pt.isec.deis.lei.pd.trabprat.client;
+}
