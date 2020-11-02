91fb27c513ccab65e2a5acf7628beadf890374c4
leandro <leandro@192.168.1.8>
2020-11-02 14:23:20 +0000

    small commit

--- CommonLibrary/src/main/java/pt/isec/deis/lei/pd/trabprat/validation/Validator.java
+++ CommonLibrary/src/main/java/pt/isec/deis/lei/pd/trabprat/validation/Validator.java
@@ -1,5 +1,8 @@
 package pt.isec.deis.lei.pd.trabprat.validation;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public final class Validator {
 
     // Validade user data (Password, Username, Name, etc.)
@@ -7,14 +10,20 @@ public final class Validator {
     private Validator() {
     }
 
-    public static boolean Name() {
-
-        return false;
+    public static boolean Name(String Name) {
+        String pattern = "[a-zA-ZáàÁÀãí ]+";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(Name);
+        //m.find();
+        return Name.equals(m.group(0));
     }
 
-    public static boolean Username() {
-
-        return false;
+    public static boolean Username(String Username) {
+        String pattern = "[a-zA-Z0-9áàÀÁ]+";
+        Pattern p = Pattern.compile(pattern);
+        Matcher m = p.matcher(Username);
+        //m.find();
+        return Username.equals(m.group(0));
     }
 
     public static boolean PasswordEquals(String Password, String ConfirmPassword) {
