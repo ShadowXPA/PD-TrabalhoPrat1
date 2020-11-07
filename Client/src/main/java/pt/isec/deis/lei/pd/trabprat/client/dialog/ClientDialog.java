81ccf8fb02522945af480671a5f697fec6593755
leandro <leandro@192.168.1.8>
2020-11-07 18:54:32 +0000

    RegisterController and ClientDialog

--- Client/src/main/java/pt/isec/deis/lei/pd/trabprat/client/dialog/ClientDialog.java
+++ Client/src/main/java/pt/isec/deis/lei/pd/trabprat/client/dialog/ClientDialog.java
@@ -1,18 +1,22 @@
 package pt.isec.deis.lei.pd.trabprat.client.dialog;
 
+import javafx.application.Platform;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 
 public final class ClientDialog {
 
-    private ClientDialog() {}
+    private ClientDialog() {
+    }
 
-    public static void ShowDialog(AlertType Alert_Type, String Title, String Header, String Description){
-        Alert alert = new Alert(Alert_Type);
-        alert.setTitle(Title);
-        alert.setHeaderText(Header);
-        alert.setContentText(Description);
+    public static void ShowDialog(AlertType Alert_Type, String Title, String Header, String Description) {
+        Platform.runLater(() -> {
+            Alert alert = new Alert(Alert_Type);
+            alert.setTitle(Title);
+            alert.setHeaderText(Header);
+            alert.setContentText(Description);
 
-        alert.showAndWait();
+            alert.showAndWait();
+        });
     }
 }
