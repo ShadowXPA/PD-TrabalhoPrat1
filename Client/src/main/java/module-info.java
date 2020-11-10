300a1a31bdf2ffa497f91c83190c425f0d7a43f2
ShadøwXPA <pedro.aves@hotmail.com>
2020-11-10 17:28:23 +0000

    PrimaryController

--- Client/src/main/java/module-info.java
+++ Client/src/main/java/module-info.java
@@ -5,4 +5,5 @@ module pt.isec.deis.lei.pd.trabprat.client {
     opens pt.isec.deis.lei.pd.trabprat.client to javafx.fxml;
     exports pt.isec.deis.lei.pd.trabprat.client;
     requires pt.isec.deis.lei.pd.trabprat;
+    requires java.base;
 }
--- Client/src/main/java/pt/isec/deis/lei/pd/trabprat/client/PrimaryController.java
+++ Client/src/main/java/pt/isec/deis/lei/pd/trabprat/client/PrimaryController.java
@@ -1,13 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package pt.isec.deis.lei.pd.trabprat.client;
 
-import java.io.IOException;
-import javafx.fxml.FXML;
-import pt.isec.deis.lei.pd.trabprat.client.App;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
 
-public class PrimaryController {
+/**
+ * FXML Controller class
+ *
+ * @author ShadowXPA
+ */
+public class PrimaryController implements Initializable {
 
-    @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
-    }
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
 }
