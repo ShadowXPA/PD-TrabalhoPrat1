bcf10f42e9b794d5ed216a5af70a63f0891e636c
shadowxpa <shadowxpa@shadowxpa-GL75-Leopard-10SEK>
2020-11-19 22:05:33 +0000

    Testing multicast

--- Server/src/main/java/pt/isec/deis/lei/pd/trabprat/server/Main.java
+++ Server/src/main/java/pt/isec/deis/lei/pd/trabprat/server/Main.java
@@ -1,7 +1,13 @@
 package pt.isec.deis.lei.pd.trabprat.server;
 
+import java.net.DatagramPacket;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.MulticastSocket;
+import java.net.NetworkInterface;
 import java.sql.SQLException;
 import java.util.Date;
+import java.util.Scanner;
 import pt.isec.deis.lei.pd.trabprat.config.DefaultConfig;
 import pt.isec.deis.lei.pd.trabprat.exception.ExceptionHandler;
 import pt.isec.deis.lei.pd.trabprat.server.config.ServerConfig;
@@ -13,40 +19,89 @@ import pt.isec.deis.lei.pd.trabprat.server.thread.udp.UDPListener;
 public class Main {
 
     public static void main(String[] args) {
-        ServerConfig SV_CFG;
-        // Catch arguments
-        // Initialize database connection
+        String IP = "239.4.5.6";
+        int Port = 5432;
         try {
-            if (args.length <= 5) {
-                System.exit(-1);
-            }
+            try ( MulticastSocket mS = new MulticastSocket(Port)) {
+                InetAddress address = InetAddress.getByName(IP);
+                NetworkInterface nI = NetworkInterface.getByInetAddress(InetAddress.getByName("10.11.14.7"));
+                mS.setNetworkInterface(nI);
+                mS.joinGroup(address);
+//                mS.joinGroup(new InetSocketAddress(address, Port), nI);
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Username: ");
+                String username = sc.nextLine();
 
-            SV_CFG = new ServerConfig(InitDatabase(args), DefaultConfig.getExternalIP(), args[5]);
-            System.out.println("External IP: " + SV_CFG.ExternalIP);
-            // Check for other servers, ask for information if there are other servers already online
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        DatagramPacket dP = new DatagramPacket(new byte[1024], 1024);
+                        try {
+                            while (true) {
+                                dP.setLength(1024);
+                                mS.receive(dP);
+                                String raw = new String(dP.getData(), 0, dP.getLength());
+                                System.out.println(dP.getAddress().getHostAddress()
+                                        + ":" + dP.getPort() + ": " + raw);
+                            }
+                        } catch (Exception ex) {
+                            ex.printStackTrace();
+                        }
+                    }
+                }).start();
 
-            // Create threads
-            // Thread Listen UDP
-            Thread tdUDP = new Thread(new UDPListener(SV_CFG), "UDPListener");
-            tdUDP.setDaemon(true);
-            tdUDP.start();
-            // Thread Listen TCP
-            Thread tdTCP = new Thread(new TCPListener(SV_CFG), "TCPListener");
-            tdTCP.setDaemon(true);
-            tdTCP.start();
-            // Thread Multicast
-            Thread tdMC = new Thread(new MulticastListener(SV_CFG), "MulticastListener");
-            tdMC.setDaemon(true);
-            tdMC.start();
-            try {
-                // Handle Admin Commands
-                new CommandLineHandler(System.in, System.out, SV_CFG).Initialize();
-            } catch (Exception ex) {
-                ExceptionHandler.ShowException(ex);
+                while (true) {
+                    String msg = sc.nextLine();
+
+                    if (msg.equals("exit")) {
+                        break;
+                    }
+
+                    msg = "[" + username + "]: " + msg;
+                    DatagramPacket dP = new DatagramPacket(msg.getBytes(), msg.getBytes().length,
+                            address, Port);
+                    mS.send(dP);
+                }
+
+                mS.leaveGroup(address);
             }
         } catch (Exception ex) {
-            ExceptionHandler.ShowException(ex);
+            ex.printStackTrace();
         }
+//        ServerConfig SV_CFG;
+//        // Catch arguments
+//        // Initialize database connection
+//        try {
+//            if (args.length <= 5) {
+//                System.exit(-1);
+//            }
+//
+//            SV_CFG = new ServerConfig(InitDatabase(args), DefaultConfig.getExternalIP(), args[5]);
+//            System.out.println("External IP: " + SV_CFG.ExternalIP);
+//            // Check for other servers, ask for information if there are other servers already online
+//
+//            // Create threads
+//            // Thread Listen UDP
+//            Thread tdUDP = new Thread(new UDPListener(SV_CFG), "UDPListener");
+//            tdUDP.setDaemon(true);
+//            tdUDP.start();
+//            // Thread Listen TCP
+//            Thread tdTCP = new Thread(new TCPListener(SV_CFG), "TCPListener");
+//            tdTCP.setDaemon(true);
+//            tdTCP.start();
+//            // Thread Multicast
+//            Thread tdMC = new Thread(new MulticastListener(SV_CFG), "MulticastListener");
+//            tdMC.setDaemon(true);
+//            tdMC.start();
+//            try {
+//                // Handle Admin Commands
+//                new CommandLineHandler(System.in, System.out, SV_CFG).Initialize();
+//            } catch (Exception ex) {
+//                ExceptionHandler.ShowException(ex);
+//            }
+//        } catch (Exception ex) {
+//            ExceptionHandler.ShowException(ex);
+//        }
     }
 
     private static Database InitDatabase(String[] args) throws SQLException, ClassNotFoundException {
