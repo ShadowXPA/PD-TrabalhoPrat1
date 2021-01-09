97295cb40692aaf10b1bf0a204544cdbd9200cdf
leandro <leandro@192.168.1.8>
2021-01-09 19:50:20 +0000

    message sen

--- Server/src/main/java/pt/isec/deis/lei/pd/trabprat/server/springboot/controllers/MessageController.java
+++ Server/src/main/java/pt/isec/deis/lei/pd/trabprat/server/springboot/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package pt.isec.deis.lei.pd.trabprat.server.springboot.controllers;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import pt.isec.deis.lei.pd.trabprat.communication.Command;
+import pt.isec.deis.lei.pd.trabprat.communication.ECommand;
+import pt.isec.deis.lei.pd.trabprat.exception.ExceptionHandler;
+import pt.isec.deis.lei.pd.trabprat.model.GenericPair;
 import pt.isec.deis.lei.pd.trabprat.model.TChannel;
+import pt.isec.deis.lei.pd.trabprat.model.TDirectMessage;
 import pt.isec.deis.lei.pd.trabprat.model.TMessage;
 import pt.isec.deis.lei.pd.trabprat.model.TUser;
 import pt.isec.deis.lei.pd.trabprat.server.Main;
@@ -19,6 +25,7 @@ import pt.isec.deis.lei.pd.trabprat.server.springboot.MainRestAPI;
 import pt.isec.deis.lei.pd.trabprat.server.springboot.interfaces.IServerService;
 import pt.isec.deis.lei.pd.trabprat.server.springboot.interfaces.ITokenService;
 import pt.isec.deis.lei.pd.trabprat.server.springboot.model.User;
+import pt.isec.deis.lei.pd.trabprat.thread.tcp.TCPHelper;
 
 @RestController
 @RequestMapping("message")
@@ -84,7 +91,46 @@ public class MessageController {
 
     @PostMapping("")
     public String sendMessage(HttpServletRequest request, @RequestBody String msg) {
+        String token = request.getHeader("Authorization");
+        var tokenSet = MainRestAPI.getToken(token);
+        if (tokenSet == null) {
+            return null;
+        }
+        if (msg.isEmpty()) {
+            return null;
+        }
+        User user = tokenSet.getKey();
+        DatabaseWrapper db = MainRestAPI.SV_CFG.DB;
+        TUser dbUser = db.getUserByUsername(user.getUsername());
+        TMessage newMessage = new TMessage(0, dbUser, msg, null, 0);
         StringBuilder sb = new StringBuilder();
+        for (var users : MainRestAPI.SV_CFG.Clients.values()) {
+            TUser userToSend = users.key;
+            TDirectMessage dm = new TDirectMessage(newMessage, userToSend);
+            int i = db.insertDirectMessage(dm.getUID(), newMessage);
+            if (i > 0) {
+                TMessage lastMessage = db.getLastMessage();
+                TDirectMessage lastDM = new TDirectMessage(lastMessage, dm.getUID());
+
+                ArrayList<TDirectMessage> dmL = db.getAllDMByUserIDAndOtherID(dm.getMID().getMUID().getUID(), dm.getUID().getUID());
+                // Broadcast new message to all users
+                ArrayList<TUser> dmU = db.getOtherUserFromDM(db.getAllDMByUserID(dm.getUID().getUID()), dm.getUID());
+                Command sendCmd = new Command(ECommand.CMD_CREATED, new GenericPair<>(dmL, dmU));
+                try {
+                    TCPHelper.SendTCPCommand(users.value, sendCmd);
+                } catch (IOException ex) {
+                    ExceptionHandler.ShowException(ex);
+                }
+                // Send through multicast
+                MainRestAPI.SV_CFG.MulticastMessage(new Command(ECommand.CMD_CREATED,
+                        new GenericPair<>(MainRestAPI.SV_CFG.ServerID, lastDM)));
+                Main.Log("[RMIClient]", "The message has been sent to '" + userToSend.getUUsername() + "'!");
+                sb.append("Message sent to: '").append(userToSend.getUName()).append("'");
+            } else {
+                Main.Log("[RMIClient]", "Message couldn´t be inserted!");
+                sb.append("Message couldn´t be sent to: '").append(userToSend.getUName()).append("'");
+            }
+        }
         return sb.toString();
     }
 }
