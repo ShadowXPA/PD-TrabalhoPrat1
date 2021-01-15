e1beaf479eab4b36094cf29c12f67734671dd413
Pedro dos Santos Alves <51165769+ShadowXPA@users.noreply.github.com>
2021-01-15 19:17:19 +0000

    Update Server2.bat

--- Server/Server2.bat
+++ Server/Server2.bat
@@ -1,1 +1,1 @@
-java -jar target/Server-1.0.0.jar 192.168.1.70 5432 5435 5436 localhost 3306 pd_trab2 pdtrab PDTrabPrat123 8081
+java -jar target/Server-1.0.0.jar [ServerIP] 5432 5435 5436 [DatabaseIP] 3306 pd_trab2 pdtrab PDTrabPrat123 8081
